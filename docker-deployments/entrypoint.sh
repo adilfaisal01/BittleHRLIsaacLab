2ce5507bbdcde990cade0a9a33ee721910a6464c
adilfaisal01 <adilfaisal01@gmail.com>
2025-10-27 13:33:48 -0400

    added correct script and task name

--- docker-deployments/entrypoint.sh
+++ docker-deployments/entrypoint.sh
@@ -5,9 +5,4 @@ set -x
 echo "🚀 Launching BittleHRL training..."
 
 # Hardcoded training script and settings, change this to match my environment implementation details
-/IsaacLab/isaaclab.sh -p /BittleHRL/scripts/skrl/train.py \
-    --task Isaac-Cartpole-v0 \
-    --num_envs 64 \
-    --headless \
-    --video \
-    --cpu-only
+/IsaacLab/isaaclab.sh -p /BittleHRL/scripts/skrl/train.py  --task Template-Bittlehrl-Direct-v0 --num_envs 2 --max_iters 500 --video --headless
