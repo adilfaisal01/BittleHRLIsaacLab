554202ec3588ec1c339758fb3ac5e1d4a2a99f0d
adilfaisal01 <adilfaisal01@gmail.com>
2025-10-27 13:54:36 -0400

    added editable entry points

--- docker-deployments/entrypoint.sh
+++ docker-deployments/entrypoint.sh
@@ -5,4 +5,8 @@ set -x
 echo "🚀 Launching BittleHRL training..."
 
 # Hardcoded training script and settings, change this to match my environment implementation details
-/IsaacLab/isaaclab.sh -p /BittleHRL/scripts/skrl/train.py  --task Template-Bittlehrl-Direct-v0 --num_envs 2 --max_iters 500 --video --headless
+/IsaacLab/isaaclab.sh -p /BittleHRL/scripts/skrl/train.py \
+  --task Template-Bittlehrl-Direct-v0 \
+  --num_envs ${NUM_ENVS:-2} \
+  --max_iters ${MAX_ITERS:-500} \
+  --video --headless
